f8aa15b1fb06aa93e71b8ca477a313440d44423b
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-23 13:16:10 +0300

    Use host builder creation helpers

--- src/NRuuviTag.Cli.Linux/Program.cs
+++ src/NRuuviTag.Cli.Linux/Program.cs
@@ -1,35 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
-
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using NRuuviTag.Cli;
 using NRuuviTag.Listener.Linux;
 
-namespace NRuuviTag.Cli.Windows {
-
-    /// <summary>
-    /// RuuviTag command-line application.
-    /// </summary>
-    public class Program {
-
-        public static async Task<int> Main(string[] args) {
-            return await CreateHostBuilder(args)
-                .BuildAndRunRuuviTagPublisher(args)
-                .ConfigureAwait(false);
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSystemd()
-                .UseContentRoot(AppContext.BaseDirectory)
-                .ConfigureAppConfiguration(config => {
-                    config.AddRuuviTagDeviceConfiguration();
-                })
-                .ConfigureServices((hostContext, services) => {
-                    services.AddRuuviTagPublisherCommandApp(hostContext.Configuration, sp => ActivatorUtilities.CreateInstance<BlueZListener>(sp, BlueZListener.DefaultBluetoothAdapter));
-                });
-
-    }
-}
+return await NRuuviTagHostBuilder.CreateHostBuilder(args, sp => ActivatorUtilities.CreateInstance<BlueZListener>(sp, BlueZListener.DefaultBluetoothAdapter))
+    .UseSystemd()
+    .BuildAndRunCommandApp(args)
+    .ConfigureAwait(false);
--- src/NRuuviTag.Cli.Windows/Program.cs
+++ src/NRuuviTag.Cli.Windows/Program.cs
@@ -1,34 +1,9 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Hosting;
 
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
+using NRuuviTag.Cli;
 using NRuuviTag.Listener.Windows;
 
-namespace NRuuviTag.Cli.Windows {
-
-    /// <summary>
-    /// RuuviTag MQTT agent command-line application.
-    /// </summary>
-    public class Program {
-
-        public static async Task<int> Main(string[] args) {
-            return await CreateHostBuilder(args)
-                .BuildAndRunRuuviTagPublisher(args)
-                .ConfigureAwait(false);
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseContentRoot(AppContext.BaseDirectory)
-                .ConfigureAppConfiguration(config => {
-                    config.AddRuuviTagDeviceConfiguration();
-                })
-                .ConfigureServices((hostContext, services) => {
-                    services.AddRuuviTagPublisherCommandApp<WindowsSdkListener>(hostContext.Configuration);
-                });
-
-    }
-}
+return await NRuuviTagHostBuilder
+    .CreateHostBuilder<WindowsSdkListener>(args)
+    .BuildAndRunCommandApp(args)
+    .ConfigureAwait(false);
