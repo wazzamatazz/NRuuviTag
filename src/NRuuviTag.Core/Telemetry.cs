426435c3e221a356363ea0e6f530d8cacaa69343
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-23 13:31:25 +0300

    Make meter name constant

--- src/NRuuviTag.Core/Telemetry.cs
+++ src/NRuuviTag.Core/Telemetry.cs
@@ -5,12 +5,18 @@ namespace NRuuviTag {
     /// <summary>
     /// Telemetry for the NRuuviTag.Core library.
     /// </summary>
-    internal static class Telemetry {
+    public static class Telemetry {
+
+        /// <summary>
+        /// The name of the meter for the NRuuviTag.Core library.
+        /// </summary>
+        public const string MeterName = "nruuvitag";
+
 
         /// <summary>
         /// The meter for the NRuuviTag.Core library.
         /// </summary>
-        public static Meter Meter { get; } = new Meter("nruuvitag", typeof(Telemetry).Assembly.GetName().Version.ToString(3));
+        public static Meter Meter { get; } = new Meter(MeterName, typeof(Telemetry).Assembly.GetName().Version.ToString(3));
 
     }
 
