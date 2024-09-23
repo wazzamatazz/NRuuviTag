0709d15db02db960bc29b12a5fe78b6fee6327da
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-23 15:18:30 +0300

    Add OpenTelemetry instrumentation library

---
+++ src/NRuuviTag.OpenTelemetry/NRuuviTagMetricsExtensions.cs
@@ -0,0 +1,30 @@
+﻿
+namespace OpenTelemetry.Metrics {
+
+    /// <summary>
+    /// Extensions for observing NRuuviTag metrics.
+    /// </summary>
+    public static class NRuuviTagMetricsExtensions {
+
+        /// <summary>
+        /// Adds NRuuviTag metrics instrumentation to the <see cref="MeterProviderBuilder"/>.
+        /// </summary>
+        /// <param name="builder">
+        ///   The <see cref="MeterProviderBuilder"/>.
+        /// </param>
+        /// <returns>
+        ///   The <see cref="MeterProviderBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   <paramref name="builder"/> is <see langword="null"/>.
+        /// </exception>
+        public static MeterProviderBuilder AddNRuuviTagInstrumentation(this MeterProviderBuilder builder) {
+            if (builder == null) {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.AddMeter(NRuuviTag.Telemetry.MeterName);
+        }
+
+    }
+}
