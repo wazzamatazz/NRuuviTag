999c750c0a8b0a71d9225353b1d41ec58887e9a6
Matias Nurmi <matias.nurmi@tuni.fi>
2024-12-18 14:19:44 +0200

    Send json to Azure

---
+++ src/NRuuviTag.AzureIotHubSender/TelemetryData.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace LinuxSdkClient {
+
+    /// <summary>
+    /// Describes a sample received from a RuuviTag sensor.
+    /// </summary>
+    /// <remarks>
+    ///   See https://docs.ruuvi.com/communication/bluetooth-advertisements for details about 
+    ///   Bluetooth LE advertisements.
+    /// </remarks>
+    /// <seealso cref="RuuviTagUtilities.CreateSampleFromPayload"/>
+    public class TelemetryData {
+
+        // <summary>
+        /// Sample time.
+        /// </summary>
+        //public DateTimeOffset? Timestamp { get; set; }
+
+        /// <summary>
+        /// Signal strength (dBm).
+        /// </summary>
+        public double? SignalStrength { get; set; }
+
+        // <summary>
+        /// Payload data format (see https://docs.ruuvi.com/communication/bluetooth-advertisements).
+        /// </summary>
+        //public byte? DataFormat { get; set; }
+
+        /// <summary>
+        /// Temperature (deg C).
+        /// </summary>
+        public double? Temperature { get; set; }
+
+        /// <summary>
+        /// Humidity (%).
+        /// </summary>
+        public double? Humidity { get; set; }
+
+        /// <summary>
+        /// Pressure (hPa).
+        /// </summary>
+        public double? Pressure { get; set; }
+
+        /// <summary>
+        /// X-acceleration (g).
+        /// </summary>
+        public double? AccelerationX { get; set; }
+
+        /// <summary>
+        /// Y-acceleration (g).
+        /// </summary>
+        public double? AccelerationY { get; set; }
+
+        /// <summary>
+        /// Z-acceleration (g).
+        /// </summary>
+        public double? AccelerationZ { get; set; }
+
+        /// <summary>
+        /// Battery voltage (V).
+        /// </summary>
+        public double? BatteryVoltage { get; set; }
+
+        /// <summary>
+        /// TX power (dBm).
+        /// </summary>
+        public double? TxPower { get; set; }
+
+        // <summary>
+        /// Movement counter (counts).
+        /// </summary>
+        //public byte? MovementCounter { get; set; }
+        
+        // <summary>
+        /// Measurement sequence.
+        /// </summary>
+        //public ushort? MeasurementSequence { get; set; }
+
+        /// <summary>
+        /// MAC address of device.
+        /// </summary>
+        public string? MacAddress { get; set; }
+
+    }
+}
--- src/NRuuviTag.AzureIotHubSender/Worker.cs
+++ src/NRuuviTag.AzureIotHubSender/Worker.cs
@@ -1,5 +1,7 @@
 using System;
-using System.Text.Json;
+//using System.Text.Json;
+using System.Text;
+using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,6 +11,9 @@ using Microsoft.Extensions.Logging;
 using NRuuviTag;
 using NRuuviTag.Listener.Linux;
 
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+
 namespace LinuxSdkClient {
     public class Worker : BackgroundService {
         private readonly ILogger<Worker> _logger;
@@ -17,17 +22,74 @@ namespace LinuxSdkClient {
             _logger = logger;
         }
 
+        class JsonMessage
+        {
+                public string temp { get; set; }
+                public int humidity { get; set; }
+        }
+
+        private static async Task<DeviceClient> ConnectToAzure() {
+            // Fetch the connection string from an environment variable
+            string DeviceConnectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+            // Create an instance of the device client using the connection string
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
+            
+            // Connect to the device client
+            await deviceClient.OpenAsync();
+            return deviceClient;
+        }
+
+        private static async Task SendToAzure(DeviceClient deviceClient, RuuviTagSample data) {
+            JsonMessage newMsg = new JsonMessage()
+            {
+                temp = "4.05",
+                humidity = 750,
+            };
+
+            string ruuvipayload = JsonConvert.SerializeObject(data);
+            string payload = JsonConvert.SerializeObject(newMsg);
+
+            Console.WriteLine(ruuvipayload);
+
+
+            // Send the data string as telemetry to Azure IoT Hub
+            Message message = new Message(Encoding.UTF8.GetBytes(ruuvipayload))
+            {
+                ContentEncoding = Encoding.UTF8.ToString(),
+                ContentType = "application/json"
+            };
+            await deviceClient.SendEventAsync(message);
+
+            // Set the data string as the latestData property in the reported properties
+            var reportedProperties = new TwinCollection();
+            reportedProperties["latestData"] = data;
+
+            await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
             try {
+                DeviceClient deviceClient = await ConnectToAzure();
                 IRuuviTagListener client = new BlueZListener("hci0");
-                JsonSerializerOptions jsonOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                //JsonSerializerOptions jsonOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
                 _logger.LogInformation("Starting RuuviTag listener.");
 
                 await foreach (var sample in client.ListenAsync(stoppingToken)) {
-                    var json = JsonSerializer.Serialize(sample, jsonOptions);
+                    //var json = JsonSerializer.Serialize(sample, jsonOptions);
 
-                    Console.WriteLine(json);
+                    /*
+                    var data = new TelemetryData{ 
+                        Temperature = sample.Temperature,
+                        Humidity = sample.Humidity,
+                        MacAddress = sample.MacAddress,
+                    };
+                    */
+
+                    await SendToAzure(deviceClient, sample);
+
+                    
                 }
             }
             catch (OperationCanceledException) { }
