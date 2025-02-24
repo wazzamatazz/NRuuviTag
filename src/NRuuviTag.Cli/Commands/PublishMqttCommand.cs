6df77420f1a4ecebb2ae2cb226aaec05c8fc2c50
Graham Watts <grahamcharleswatts@gmail.com>
2025-02-24 21:24:44 +0200

    Remove unused namespaces

--- src/NRuuviTag.Cli/Commands/PublishMqttCommand.cs
+++ src/NRuuviTag.Cli/Commands/PublishMqttCommand.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
