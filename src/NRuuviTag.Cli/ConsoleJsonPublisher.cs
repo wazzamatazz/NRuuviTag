49ea621ebed5dd9ee2125c8b227db1c806c5f082
Graham Watts <grahamcharleswatts@gmail.com>
2025-10-03 15:59:50 +0300

    Improve JSON output and device scan robustness
    Updated project descriptions to refer to 'Ruuvi IoT sensors' instead of 'RuuviTag IoT sensors'. Container image tags now distinguish prerelease versions. DeviceScanCommand null-checks samples before accessing MacAddress. ConsoleJsonPublisher uses cancellation tokens for async iteration, ignores nulls in JSON output, and directly instantiates RuuviTagSampleExtended for serialization.

--- src/NRuuviTag.Cli/ConsoleJsonPublisher.cs
+++ src/NRuuviTag.Cli/ConsoleJsonPublisher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,7 +11,8 @@ namespace NRuuviTag.Cli {
         private readonly Func<string, Device?>? _getDeviceInfo;
 
         private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions() { 
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
         };
 
 
@@ -24,12 +26,12 @@ namespace NRuuviTag.Cli {
 
 
         protected override async Task RunAsync(IAsyncEnumerable<RuuviTagSample> samples, CancellationToken cancellationToken) {
-            await foreach (var item in samples.ConfigureAwait(false)) {
+            await foreach (var item in samples.WithCancellation(cancellationToken).ConfigureAwait(false)) {
                 var knownDevice = _getDeviceInfo?.Invoke(item.MacAddress!);
 
                 var json = knownDevice == null
                     ? JsonSerializer.Serialize(item, _jsonOptions)
-                    : JsonSerializer.Serialize(RuuviTagSampleExtended.Create(item, knownDevice.DeviceId, knownDevice.DisplayName), _jsonOptions);
+                    : JsonSerializer.Serialize(new RuuviTagSampleExtended(knownDevice.DeviceId, knownDevice.DisplayName, item), _jsonOptions);
 
                 Console.WriteLine(json);
             }
