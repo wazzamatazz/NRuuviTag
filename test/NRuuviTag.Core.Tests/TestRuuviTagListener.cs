41796dcfa1155d219bc2cdc906ddf794a37154ac
Graham Watts <grahamcharleswatts@gmail.com>
2025-11-12 21:26:48 +0200

    Namespace cleanup

--- test/NRuuviTag.Core.Tests/TestRuuviTagListener.cs
+++ test/NRuuviTag.Core.Tests/TestRuuviTagListener.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
