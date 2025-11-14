1971142898636b273301ad183198bbd05fd6be74
Graham Watts <grahamcharleswatts@gmail.com>
2025-11-14 22:44:15 +0200

    Add method for binding settings to listener options

--- src/NRuuviTag.Cli/Commands/ListenerCommandSettings.cs
+++ src/NRuuviTag.Cli/Commands/ListenerCommandSettings.cs
@@ -9,5 +9,15 @@ public abstract class ListenerCommandSettings : CommandSettings {
     [CommandOption("--enable-data-format-6")]
     [Description("Specifies whether Data Format 6 advertisements should be processed. By default, Data Format 6 advertisements are ignored in preference of Data Format E1 advertisements.")]
     public bool EnableDataFormat6 { get; set; }
+    
+    [CommandOption("--allow-duplicate-advertisements")]
+    [Description("Linux only. Specifies whether duplicate advertisements should be allowed. By default, duplicate advertisements are ignored.")]
+    public bool AllowDuplicateAdvertisements { get; set; }
 
+
+    internal virtual void Bind(RuuviTagListenerOptions options) {
+        options.EnableDataFormat6 = EnableDataFormat6;
+        options.AllowDuplicateAdvertisements = AllowDuplicateAdvertisements;
+    }
+    
 }
