272e8d23c82d1574db1de602a9361f734589fe58
Graham Watts <grahamcharleswatts@gmail.com>
2025-11-14 13:33:32 +0200

    Add options flag to control duplicate advertisement handling

--- src/NRuuviTag.Listener.Linux/BlueZListenerOptions.cs
+++ src/NRuuviTag.Listener.Linux/BlueZListenerOptions.cs
@@ -9,5 +9,10 @@ public class BlueZListenerOptions : RuuviTagListenerOptions {
     /// The name of the Bluetooth adapter to use.
     /// </summary>
     public string AdapterName { get; set; } = BlueZListener.DefaultBluetoothAdapter;
+    
+    /// <summary>
+    /// Specifies if duplicate advertisements are allowed.
+    /// </summary>
+    public bool AllowDuplicateAdvertisements { get; set; }
 
 }
