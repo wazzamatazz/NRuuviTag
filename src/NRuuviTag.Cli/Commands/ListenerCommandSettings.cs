9f398ea7480b3915245a15d1c8f0553311bc7ec7
Graham Watts <grahamcharleswatts@gmail.com>
2025-11-26 15:17:02 +0200

    Update base command settings

--- src/NRuuviTag.Cli/Commands/ListenerCommandSettings.cs
+++ src/NRuuviTag.Cli/Commands/ListenerCommandSettings.cs
@@ -5,10 +5,10 @@ using Spectre.Console.Cli;
 namespace NRuuviTag.Cli.Commands;
 
 public abstract class ListenerCommandSettings : CommandSettings {
-
-    [CommandOption("--enable-data-format-6")]
-    [Description("Specifies whether Data Format 6 advertisements should be processed. By default, Data Format 6 advertisements are ignored in preference of Data Format E1 advertisements.")]
-    public bool EnableDataFormat6 { get; set; }
+    
+    [CommandOption("--extended-advertisements")]
+    [Description("Specifies whether extended advertisement formats should be enabled. When enabled, formats that use extended advertisements will be processed, and their fallback formats will be ignored.")]
+    public bool EnableExtendedAdvertisementFormats { get; set; }
     
     [CommandOption("--allow-duplicate-advertisements")]
     [Description("Linux only. Specifies whether duplicate advertisements should be allowed. By default, duplicate advertisements are ignored.")]
@@ -16,7 +16,7 @@ public abstract class ListenerCommandSettings : CommandSettings {
 
 
     internal virtual void Bind(RuuviTagListenerOptions options) {
-        options.EnableDataFormat6 = EnableDataFormat6;
+        options.EnableExtendedAdvertisementFormats = options.EnableExtendedAdvertisementFormats;
         options.AllowDuplicateAdvertisements = AllowDuplicateAdvertisements;
     }
     
