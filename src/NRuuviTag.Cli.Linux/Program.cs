3d9d23423475e8f3377e51f54fff1efdcb29c526
Graham Watts <grahamcharleswatts@gmail.com>
2026-07-11 23:34:20 +0300

    Add platform-specific data directory resolvers (#82)

--- src/NRuuviTag.Cli.Linux/Program.cs
+++ src/NRuuviTag.Cli.Linux/Program.cs
@@ -3,7 +3,7 @@
 using NRuuviTag.Cli;
 using NRuuviTag.Cli.Linux;
 
-return await NRuuviTagHostBuilder.CreateHostBuilder<BlueZListenerFactory>(args)
+return await NRuuviTagHostBuilder.CreateHostBuilder<BlueZListenerFactory>(args, new LinuxDirectoryResolver())
     .UseSystemd()
     .BuildHostAndRunNRuuviTagAsync(args)
     .ConfigureAwait(false);
